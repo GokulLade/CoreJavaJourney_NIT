a8939158ab3e85e53f84efdf9539659b925b5904
Gokul <gokullade89@gmail.com>
2024-11-01 23:36:44 +0530

    Object class method calling using Interface.

---
+++ src/com/nareshit/C_object_oriented_programming/G_abstraction_withjava8features/B_interfaceprograms/ObjectClassMethodInInterfaceProgram10.java
@@ -0,0 +1,20 @@
+package com.nareshit.C_object_oriented_programming.G_abstraction_withjava8features.B_interfaceprograms;
+
+// Object class Method calling using interface
+interface Acceptable1 
+{
+	
+}
+
+public class ObjectClassMethodInInterfaceProgram10 {
+
+	public static void main(String[] args) 
+	{
+		Acceptable1 a=null;
+		
+		a.hashCode();
+		a.toString();
+		a.equals(a);
+		
+	}
+}
